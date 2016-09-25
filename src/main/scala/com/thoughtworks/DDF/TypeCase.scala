b7896f11bce6245343199f46d27de3af380225b1
Marisa Kirisame <lolisa@marisa.moe>
2016-09-25 12:53:55 -0700

    make jmeq to eq

--- src/main/scala/com/thoughtworks/DDF/TypeCase.scala
+++ src/main/scala/com/thoughtworks/DDF/TypeCase.scala
@@ -5,5 +5,5 @@ import scalaz.Leibniz._
 trait TypeCase[Self[Y] <: TypeCase[Self, Y], X] {
   type ret
 
-  def unique[Y](tc: Self[Y])(implicit ev: X === Y): ret === tc.ret = /*enforce by user*/ force[Nothing, Any, ret, tc.ret]
+  def unique[Y](tc: Self[Y]): ret === tc.ret = /*enforce by user*/ force[Nothing, Any, ret, tc.ret]
 }
