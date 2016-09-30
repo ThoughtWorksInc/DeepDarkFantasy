d68f580bb670c1d66c6f7128bc58c9a043fed67b
Marisa Kirisame <lolisa@marisa.moe>
2016-09-30 05:26:33 -0700

    refactor code

--- src/main/scala/com/thoughtworks/DDF/Eval.scala
+++ src/main/scala/com/thoughtworks/DDF/Eval.scala
@@ -1,5 +1,35 @@
 package com.thoughtworks.DDF
 
+import scalaz.Monoid
+
+trait Loss[X] extends TypeCase[Loss, X] {
+  final type loss = ret
+
+  def m: Monoid[loss]
+
+  def conv: /*loss backprop ability*/ X => Eval[X]
+
+  val lc: LossCase[X]
+
+  def lca: lc.ret
+}
+
+object Loss {
+  type Aux[X, XL] = Loss[X] {type ret = XL}
+}
+
+trait LossCase[X] extends TypeCase[LossCase, X]
+
+object LossCase {
+  type Aux[X, Y] = LossCase[X] {type ret = Y}
+}
+
+trait EvalCase[X] extends TypeCase[EvalCase, X]
+
+object EvalCase {
+  type Aux[X, Y] = EvalCase[X] {type ret = Y}
+}
+
 trait Eval[X] {
   val loss: Loss[X]
 
--- src/main/scala/com/thoughtworks/DDF/EvalCase.scala
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.DDF
-
-trait EvalCase[X] extends TypeCase[EvalCase, X]
-
-object EvalCase {
-  type Aux[X, Y] = EvalCase[X] {type ret = Y}
-}
