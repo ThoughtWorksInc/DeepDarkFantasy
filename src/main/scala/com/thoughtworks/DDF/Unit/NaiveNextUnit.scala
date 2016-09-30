89a0a730683169caf610c7402145a0bb94387105
Marisa Kirisame <lolisa@marisa.moe>
2016-09-30 16:09:51 -0700

    add implicit def for NaiveNextUnit

--- src/main/scala/com/thoughtworks/DDF/Unit/NaiveNextUnit.scala
+++ src/main/scala/com/thoughtworks/DDF/Unit/NaiveNextUnit.scala
@@ -11,3 +11,15 @@ trait NaiveNextUnit[Info[_], Repr[_], Arg] extends
 
   override implicit def UnitInfo: Info[Arg => Unit] = iconv(base.UnitInfo)
 }
+
+object NaiveNextUnit {
+  implicit def apply[Info[_], Repr[_], Arg]
+  (implicit unitl: UnitLang[Info, Repr], skil: SKILang[Info, Repr], arg: Info[Arg]) =
+    new NaiveNextUnit[Info, Repr, Arg] {
+      override implicit def base: UnitLang[Info, Repr] = unitl
+
+      override implicit def argi: Info[Arg] = arg
+
+      override implicit def ski: SKILang[Info, Repr] = skil
+  }
+}
