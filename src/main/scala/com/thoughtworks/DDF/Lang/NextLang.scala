13683e764fe0c1688163a41a5da0c277a08fa75b
Marisa Kirisame <lolisa@marisa.moe>
2016-10-05 03:34:25 -0700

    refactor code

--- src/main/scala/com/thoughtworks/DDF/Lang/NextLang.scala
+++ src/main/scala/com/thoughtworks/DDF/Lang/NextLang.scala
@@ -2,6 +2,7 @@ package com.thoughtworks.DDF.Lang
 
 import com.thoughtworks.DDF.Combinators.{NextComb, SKIRepr}
 import com.thoughtworks.DDF.Double.NextDouble
+import com.thoughtworks.DDF.List.NextList
 import com.thoughtworks.DDF.NextBase
 import com.thoughtworks.DDF.Option.NextOption
 import com.thoughtworks.DDF.Product.NextProduct
@@ -14,27 +15,13 @@ trait NextLang[Info[_], Repr[_], Arg] extends
   NextDouble[Info, Repr, Arg] with
   NextProduct[Info, Repr, Arg] with
   NextOption[Info, Repr, Arg] with
-  NextSum[Info, Repr, Arg] {
+  NextSum[Info, Repr, Arg] with
+  NextList[Info, Repr, Arg] {
   implicit def base: Lang[Info, Repr]
-
-  override implicit def listInfo[A](implicit ai: Info[Arg => A]) = iconv(base.listInfo(base.arrowRangeInfo(ai)))
-
-  override def listElmInfo[A](implicit lai: Info[Arg => List[A]]) = iconv(base.listElmInfo(base.arrowRangeInfo(lai)))
 
   override implicit def unitInfo = iconv(base.unitInfo)
 
   override def mkUnit = rconv(base.mkUnit)
-
-  override def nil[A](implicit ai: Info[Arg => A]) = rconv(base.nil(base.arrowRangeInfo(ai)))
-
-  override def cons[A](implicit ai: Info[Arg => A]) =
-    rconv(base.cons(base.arrowRangeInfo(ai)))
-
-  override def listMatch[A, B](implicit ai: Info[Arg => A], bi: Info[Arg => B]) =
-    rconv(base.listMatch(base.arrowRangeInfo(ai), base.arrowRangeInfo(bi)))
-
-  override def listMap[A, B](implicit ai: Info[Arg => A], bi: Info[Arg => B]) =
-    rconv(base.listMap(base.arrowRangeInfo(ai), base.arrowRangeInfo(bi)))
 
   override def litB = b => rconv(base.litB(b))
 
---
+++ src/main/scala/com/thoughtworks/DDF/List/NextList.scala
@@ -0,0 +1,38 @@
+package com.thoughtworks.DDF.List
+
+import com.thoughtworks.DDF.Arrow.{ArrowRepr, NextArrow}
+import com.thoughtworks.DDF.Combinators.SKIRepr
+
+trait NextList [Info[_], Repr[_], Arg] extends
+  ListRepr[Lambda[X => Info[Arg => X]], Lambda[X => Either[Repr[X], Repr[Arg => X]]]] with
+  NextArrow[Info, Repr, Arg] {
+  override implicit def base: ListRepr[Info, Repr]
+
+  override implicit def listInfo[A](implicit ai: Info[Arg => A]) = iconv(base.listInfo(convi(ai)))
+
+  override def listElmInfo[A](implicit lai: Info[Arg => List[A]]) = iconv(base.listElmInfo(convi(lai)))
+
+  override def nil[A](implicit ai: Info[Arg => A]) = rconv(base.nil(convi(ai)))
+
+  override def cons[A](implicit ai: Info[Arg => A]) = rconv(base.cons(convi(ai)))
+
+  override def listMatch[A, B](implicit ai: Info[Arg => A], bi: Info[Arg => B]) =
+    rconv(base.listMatch(convi(ai), convi(bi)))
+
+  override def listMap[A, B](implicit ai: Info[Arg => A], bi: Info[Arg => B]) =
+    rconv(base.listMap(convi(ai), convi(bi)))
+}
+
+object NextList {
+  implicit def apply[Info[_], Repr[_], Arg](implicit
+                                            list: ListRepr[Info, Repr],
+                                            skir: SKIRepr[Info, Repr],
+                                            arg: Info[Arg])  =
+    new NextList[Info, Repr, Arg] {
+      override implicit def base: ListRepr[Info, Repr] = list
+
+      override implicit def ski: SKIRepr[Info, Repr] = skir
+
+      override implicit def argi: Info[Arg] = arg
+  }
+}
