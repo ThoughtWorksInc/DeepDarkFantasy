d19e6c1c772769918734e739540352cff574866a
Marisa Kirisame <lolisa@marisa.moe>
2016-10-23 19:03:55 -0700

    Fix it

--- src/main/scala/com/thoughtworks/DDF/Double/BEvalDoubleInfo.scala
+++ src/main/scala/com/thoughtworks/DDF/Double/BEvalDoubleInfo.scala
@@ -6,45 +6,45 @@ import com.thoughtworks.DDF.{BEval, BEvalCase, CommutativeMonoid, Loss, LossCase
 import scalaz.Leibniz._
 
 trait BEvalDoubleInfo extends BEvalArrowInfo with DoubleInfo[Loss, BEval] {
-  case class DLoss(d: Double)
+  case class DLoss(d: scala.Double)
 
-  object DLC extends LossCase[Double] {
+  object DLC extends LossCase[scala.Double] {
     override type ret = Unit
   }
 
-  override implicit def doubleInfo: Loss.Aux[Double, DLoss] = new Loss[Double] {
+  override implicit def doubleInfo: Loss.Aux[scala.Double, DLoss] = new Loss[scala.Double] {
     override def m: CommutativeMonoid[DLoss] = new CommutativeMonoid[DLoss] {
       override def zero: DLoss = DLoss(0)
 
       override def append(f1: DLoss, f2: => DLoss): DLoss = DLoss(f1.d + f2.d)
     }
 
-    override def convert: Double => BEval[Double] = dEval
+    override def convert: scala.Double => BEval[scala.Double] = dEval
 
-    override val lc: LossCase.Aux[Double, DLC.ret] = DLC
+    override val lc: LossCase.Aux[scala.Double, DLC.ret] = DLC
 
     override def lca: lc.ret = ()
 
     override type ret = DLoss
 
-    override def update(x: Double)(rate: Double)(l: loss): Double = x - l.d * rate
+    override def update(x: scala.Double)(rate: scala.Double)(l: loss): scala.Double = x - l.d * rate
   }
 
-  object DoubleBEC extends BEvalCase[Double] {
-    override type ret = Double
+  object DoubleBEC extends BEvalCase[scala.Double] {
+    override type ret = scala.Double
   }
 
-  def dEval(d: Double) = new BEval[Double] {
-    override val loss: Loss[Double] = doubleInfo
+  def dEval(d: scala.Double) = new BEval[scala.Double] {
+    override val loss: Loss[scala.Double] = doubleInfo
 
-    override def eval: Double = d
+    override def eval: scala.Double = d
 
-    override val ec: BEvalCase.Aux[Double, Double] = DoubleBEC
+    override val ec: BEvalCase.Aux[scala.Double, scala.Double] = DoubleBEC
 
     override def eca: ec.ret = d
   }
 
-  def deval(d: BEval[Double]): Double = witness(d.ec.unique(DoubleBEC))(d.eca)
+  def deval(d: BEval[scala.Double]): scala.Double = witness(d.ec.unique(DoubleBEC))(d.eca)
 }
 
 object BEvalDoubleInfo {
