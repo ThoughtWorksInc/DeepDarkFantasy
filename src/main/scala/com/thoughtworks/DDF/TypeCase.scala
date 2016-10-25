783e9671083a7720c6d1edf33ce198e909226ec7
Marisa Kirisame <lolisa@marisa.moe>
2016-10-24 21:05:11 -0700

    fix fucking bug

--- src/main/scala/com/thoughtworks/DDF/TypeCase.scala
+++ src/main/scala/com/thoughtworks/DDF/TypeCase.scala
@@ -5,5 +5,5 @@ import scalaz.Leibniz._
 trait TypeCase[Self[Y] <: TypeCase[Self, Y], X] {
   type ret
 
-  def unique[Y](tc: Self[Y]): ret === tc.ret = /*enforce by user*/ force[Nothing, Any, ret, tc.ret]
+  def unique(tc: Self[X]): ret === tc.ret = /*enforce by user*/ force[Nothing, Any, ret, tc.ret]
 }
