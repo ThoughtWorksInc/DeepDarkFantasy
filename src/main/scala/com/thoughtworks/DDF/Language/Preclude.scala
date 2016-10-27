74455ed8c9f901768966d5ac3f228d685c1b99e0
Marisa Kirisame <lolisa@marisa.moe>
2016-10-26 18:50:23 -0700

    simplify preclude

--- src/main/scala/com/thoughtworks/DDF/Language/Preclude.scala
+++ src/main/scala/com/thoughtworks/DDF/Language/Preclude.scala
@@ -3,21 +3,32 @@ package com.thoughtworks.DDF.Language
 object Preclude {
   def square[Info[_], Repr[_]](implicit lang: Lang[Info, Repr]): Repr[Double => Double] = lang.W_(lang.multD)
 
+  def square_[Info[_], Repr[_]](r: Repr[Double])(implicit lang: Lang[Info, Repr]): Repr[Double] =
+    lang.app(square(lang))(r)
+
   def sumList[Info[_], Repr[_]](implicit lang: Lang[Info, Repr]): Repr[List[Double] => Double] = {
     val nLang = NextLang(lang, lang.listInfo(lang.doubleInfo))
-    import nLang._
-    collapse(foldLeft___(plusD)(litD(0))(in))
+    nLang.collapse(sumList_[nLang.info, nLang.repr](nLang.in)(nLang))
   }
 
-  def sumList_[Info[_], Repr[_]](li: Repr[List[Double]])(implicit lang: Lang[Info, Repr]) =
-    lang.app(sumList)(li)
+  def sumList_[Info[_], Repr[_]](li: Repr[List[Double]])(implicit lang: Lang[Info, Repr]) = {
+    import lang._
+    foldLeft___(plusD)(litD(0))(li)
+  }
+
+  def dot__[Info[_], Repr[_]](l: Repr[List[Double]])(r: Repr[List[Double]])(implicit lang: Lang[Info, Repr]):
+  Repr[Double] = {
+    import lang._
+    sumList_(listMap__(uncurry_(multD))(listZip__(l)(r)))
+  }
+
+  def dot_[Info[_], Repr[_]](l: Repr[List[Double]])(implicit lang: Lang[Info, Repr]): Repr[List[Double] => Double] = {
+    val nLang = NextLang(lang, lang.listInfo(lang.doubleInfo))
+    nLang.collapse(dot__[nLang.info, nLang.repr](nLang.rconv(l))(nLang.in)(nLang))
+  }
 
   def dot[Info[_], Repr[_]](implicit lang: Lang[Info, Repr]): Repr[List[Double] => List[Double] => Double] = {
     val nLang = NextLang(lang, lang.listInfo(lang.doubleInfo))
-    val nNLang =
-      NextLang.apply[nLang.info, nLang.repr, List[Double]](nLang, nLang.listInfo(nLang.doubleInfo))
-    import nNLang._
-    nLang.collapse(collapse(sumList_[nNLang.info, nNLang.repr](
-      listMap__(uncurry_(multD))(listZip__(rconv(nLang.in))(in)))(nNLang)))
+    nLang.collapse(dot_[nLang.info, nLang.repr](nLang.in)(nLang))
   }
 }
