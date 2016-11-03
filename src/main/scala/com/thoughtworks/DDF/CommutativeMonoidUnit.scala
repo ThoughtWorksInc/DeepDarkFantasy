492ea221f6aee14765ede8b9982f53068ba07909
Marisa Kirisame <lolisa@marisa.moe>
2016-11-03 05:22:27 -0700

    remove beval

--- src/main/scala/com/thoughtworks/DDF/CommutativeMonoidUnit.scala
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.DDF
-
-trait CommutativeMonoidUnit extends CommutativeMonoid[Unit] {
-  override def zero: Unit = ()
-
-  override def append(f1: Unit, f2: => Unit): Unit = ()
-}
-
-object CommutativeMonoidUnit extends CommutativeMonoidUnit
