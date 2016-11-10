d0ece95fa88a096b9ec1534cc4cf1ac5fce2a772
Marisa Kirisame <lolisa@marisa.moe>
2016-11-10 11:16:04 -0800

    plus is good

--- src/main/scala/com/thoughtworks/DDF/Gradient/GDouble.scala
+++ src/main/scala/com/thoughtworks/DDF/Gradient/GDouble.scala
@@ -6,15 +6,31 @@ trait GDouble extends Gradient[Double] {
 
   override def constG: LangTerm[Double] = ltl.litD(0)
 
-  override def plus: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = ???
+  def lift: LangTerm[Double => Double] =>
+    LangTerm[((Double, Double)) => Double] =>
+      LangTerm[((Double, Double)) => (Double, Double)] = origf => gradf => {
+    ltl.S__(ltl.B__(ltl.mkProd)(ltl.B__(origf)(ltl.zro[Double, Double])))(gradf)
+  }
 
-  override def mult: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = ???
+  def lift2: LangTerm[Double => Double => Double] =>
+    LangTerm[((Double, Double)) => ((Double, Double)) => Double] =>
+      LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = origf => gradf => {
+    ltl.curry_(ltl.S__(
+      ltl.B__(ltl.mkProd)(
+        ltl.uncurry_(ltl.C_(ltl.B__(ltl.C_(ltl.B__(origf)(ltl.zro[Double, Double])))(ltl.zro[Double, Double])))))(
+      ltl.uncurry_(gradf)))
+  }
 
-  override def div: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = ???
+  override def plus: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] =
+    lift2(ltl.plusD)(ltl.B__(ltl.C_(ltl.B__(ltl.plusD)(ltl.fst[Double, Double])))(ltl.fst[Double, Double]))
 
-  override def sig: LangTerm[((Double, Double)) => (Double, Double)] = ???
+  override def mult: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = lift2(ltl.multD)(???)
 
-  override def exp: LangTerm[((Double, Double)) => (Double, Double)] = ???
+  override def div: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = lift2(ltl.divD)(???)
+
+  override def sig: LangTerm[((Double, Double)) => (Double, Double)] = lift(ltl.sigD)(???)
+
+  override def exp: LangTerm[((Double, Double)) => (Double, Double)] = lift(ltl.expD)(???)
 }
 
 object GDouble extends GDouble
