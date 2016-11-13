ec29dc4231ddfc6a97523791b8bdc2b189be7a60
Marisa Kirisame <lolisa@marisa.moe>
2016-11-13 02:42:19 -0800

    add div, exp

--- src/main/scala/com/thoughtworks/DDF/Gradient/GDouble.scala
+++ src/main/scala/com/thoughtworks/DDF/Gradient/GDouble.scala
@@ -32,11 +32,23 @@ trait GDouble extends Gradient[Double] {
         r.plusD__(r.multD__(r.zro_(r.rconv(l.in)))(r.fst_(r.in)))(r.multD__(r.zro_(r.in))(r.fst_(r.rconv(l.in))))))
     })
 
-  override def div: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] = lift2(ltl.divD)(???)
+  override def div: LangTerm[((Double, Double)) => ((Double, Double)) => (Double, Double)] =
+    lift2(ltl.divD)({
+      val l = NextLang.apply[LangInfoG, LangTerm, (Double, Double)](ltl, ltl.prodInfo(ltl.doubleInfo, ltl.doubleInfo))
+      val r = NextLang.apply[LangInfoG, l.repr, (Double, Double)](l, ltl.prodInfo(ltl.doubleInfo, ltl.doubleInfo))
+      l.collapse(r.collapse(
+        r.plusD__(
+          r.divD__(r.fst_(r.rconv(l.in)))(r.zro_(r.in)))(
+          r.multD__(r.litD(-1))(r.divD__(r.zro_(r.rconv(l.in)))(r.multD__(r.fst_(r.in))(r.fst_(r.in)))))))
+    })
 
   override def sig: LangTerm[((Double, Double)) => (Double, Double)] = lift(ltl.sigD)(???)
 
-  override def exp: LangTerm[((Double, Double)) => (Double, Double)] = lift(ltl.expD)(???)
+  override def exp: LangTerm[((Double, Double)) => (Double, Double)] =
+    lift(ltl.expD)(
+      ltl.S__[(Double, Double), Double, Double](
+        ltl.B__(ltl.multD)(ltl.zro[Double, Double]))(
+        ltl.fst[Double, Double]))
 }
 
 object GDouble extends GDouble
