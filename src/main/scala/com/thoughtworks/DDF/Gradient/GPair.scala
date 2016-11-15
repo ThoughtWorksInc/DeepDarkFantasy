eb35a7f4cc1a219aabb7f89efff1320ffb41d69d
Marisa Kirisame <lolisa@marisa.moe>
2016-11-15 05:29:53 -0800

    impl mult and plus

--- src/main/scala/com/thoughtworks/DDF/Gradient/GPair.scala
+++ src/main/scala/com/thoughtworks/DDF/Gradient/GPair.scala
@@ -1,5 +1,5 @@
 package com.thoughtworks.DDF.Gradient
-import com.thoughtworks.DDF.Language.{LangInfoG, LangTerm, NextLang}
+import com.thoughtworks.DDF.Language.{LangInfoG, LangTerm, LangTermLang, NextLang}
 
 trait GPair[A, B] extends Gradient[(A, B)] {
   def combine[C]: Stream[C] => Stream[C] => Stream[C] = {
@@ -31,9 +31,22 @@ trait GPair[A, B] extends Gradient[(A, B)] {
       override val gd: LangTerm[((A, B)) => Double] = ltl.B__(x.gd)(ltl.fst[A, B])
     }))
 
-  override def mult: LangTerm[(Double) => ((A, B)) => (A, B)] = ???
+  override def mult: LangTerm[Double => ((A, B)) => (A, B)] =
+    ltl.C_[(A, B), Double, (A, B)]({
+      val p = NextLang.apply[LangInfoG, LangTerm, (A, B)]
+      p.collapse(
+        p.S__[Double, B, (A, B)](
+          p.B__(p.mkProd[A, B])(p.B__(p.Let_[A, A](p.zro_(p.in)): p.repr[(A => A) => A])(p.rconv(AG.mult))))(
+          p.B__(p.Let_[B, B](p.fst_(p.in)))(p.rconv(BG.mult))))
+    })
 
-  override def plus: LangTerm[((A, B)) => ((A, B)) => (A, B)] = ???
+  override def plus: LangTerm[((A, B)) => ((A, B)) => (A, B)] = {
+    implicit val l = NextLang.apply[LangInfoG, LangTerm, (A, B)]
+    val r = NextLang.apply[LangInfoG, l.repr, (A, B)]
+    l.collapse(r.collapse(r.mkProd__(
+      r.app(r.app(r.rconv(l.rconv(AG.plus)))(r.zro_(r.rconv(l.in))))(r.zro_(r.in)))(
+      r.app(r.app(r.rconv(l.rconv(BG.plus)))(r.fst_(r.rconv(l.in))))(r.fst_(r.in)))))
+  }
 }
 
 object GPair {
